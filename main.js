d94751b5fed5b9510a5ffb8bf06bbb5c7577c0bb
Eric Coleman <ecoleman@pillartechnology.com>
2016-10-13 22:35:34 -0400

    idk just staring at screen thinking

---
+++ main.js
@@ -0,0 +1,6 @@
+var myShip = {
+                crew: [{name: "Eric"}],
+                engine: { type: "shitty", charge: 1},
+                gun: { type: "crappy", damage: 10, accuracy: .5}
+              }
+              }
