66e339368ee4ce120c9a0c5f8b5d0815e03485a7
Eric Coleman <ecoleman@pillartechnology.com>
2016-10-13 23:58:52 -0400

    Rudimentary interface

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-var myShip = {
-                crew: [{name: "Eric"}],
-                engine: { type: "shitty", charge: 1},
-                gun: { type: "crappy", damage: 10, accuracy: .5}
-              }
-              }
+var myShip =
+              { crew: [{name: "Eric"}],
+                engine: { type: "shitty", charge: 1, speed: 10 },
+                gun: { type: "crappy", chargeCapacity: 1, damage: 10, accuracy: .5 },
+                hull: 10
+                position: {x: 0, y: 0}}
+// battleships confide in me and tell me where you are
+
+update(){
+  myShip.position.x += charge;
+}
